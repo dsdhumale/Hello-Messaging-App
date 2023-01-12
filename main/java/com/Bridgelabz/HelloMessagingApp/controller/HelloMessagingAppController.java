b8e91aedea9ba4bef89dfb4d36a80c43c7850c7f
dsdhumale <d.sdhumale@gmail.com>
2023-01-12 17:59:31 +0530

    [Devendra]Added: REST API for put request method in Hello Messaging App

--- main/java/com/Bridgelabz/HelloMessagingApp/controller/HelloMessagingAppController.java
+++ main/java/com/Bridgelabz/HelloMessagingApp/controller/HelloMessagingAppController.java
@@ -3,6 +3,7 @@ package com.Bridgelabz.HelloMessagingApp.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,25 +13,35 @@ import com.Bridgelabz.HelloMessagingApp.controller.dto.User;
 @RestController
 @RequestMapping("/hello")
 
+//Url : http://localhost:8080/hello
 public class HelloMessagingAppController {
     @GetMapping("")
     public String hello(){
         return "Hello from Bridgelabz";
     }
 
+//Url : http://localhost:8080/hello/query?name=Devendra
     @GetMapping("/query")
     public String queryParam(@RequestParam String name){
         return "Hello " + name + " from Bridgelabz" ;
     }
 
+//Url : http://localhost:8080/hello/path/Devendra
     @GetMapping("/path/{name}")
     public String nameParam(@PathVariable String name){
         return "Hello " + name + " !!!!" ;
     }
 
-    @PostMapping("/user")
-    public String addUser(@RequestBody User user) {
+//Url : http://localhost:8080/hello/post    
+    @PostMapping("/post")
+    public String userName(@RequestBody User user) {
         return "Hello " + user.getFirstName() + " " + user.getLastName() +"!";
+    }
+
+//Url : http://localhost:8080/hello/put/Devendra/?lastName=Dhumale    
+    @PutMapping("/put/{firstName}/")
+    public String updateName(@PathVariable String firstName,@RequestParam String lastName){
+        return "Hello " + firstName + " " + lastName + "!!";
     }
 
 }
