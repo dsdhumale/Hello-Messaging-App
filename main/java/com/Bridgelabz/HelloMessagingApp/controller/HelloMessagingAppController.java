6450bbac506feb51327bdd7bb8e47a8c334650ba
dsdhumale <d.sdhumale@gmail.com>
2023-01-12 16:19:47 +0530

    [Devendra]Added: REST API for call path variable in Hello Messaging App

--- main/java/com/Bridgelabz/HelloMessagingApp/controller/HelloMessagingAppController.java
+++ main/java/com/Bridgelabz/HelloMessagingApp/controller/HelloMessagingAppController.java
@@ -1,6 +1,7 @@
 package com.Bridgelabz.HelloMessagingApp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,5 +20,12 @@ public class HelloMessagingAppController {
         return "Hello " + name + " from Bridgelabz" ;
 
     }
+
+    @GetMapping("/path/{name}")
+    public String nameParam(@PathVariable String name){
+        return "Hello " + name + " !!!!" ;
+
+    }
+
 }
 
