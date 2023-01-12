c4ad315354929159b4ad88d62287d7be4d6b7db8
dsdhumale <d.sdhumale@gmail.com>
2023-01-12 16:12:29 +0530

    [Devendra]Added: REST API for call query param in Hello Messaging App

--- main/java/com/Bridgelabz/HelloMessagingApp/controller/HelloMessagingAppController.java
+++ main/java/com/Bridgelabz/HelloMessagingApp/controller/HelloMessagingAppController.java
@@ -2,6 +2,7 @@ package com.Bridgelabz.HelloMessagingApp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,6 +12,12 @@ public class HelloMessagingAppController {
     @GetMapping("")
     public String hello(){
         return "Hello from Bridgelabz";
+    }
+
+    @GetMapping("/query")
+    public String queryParam(@RequestParam String name){
+        return "Hello " + name + " from Bridgelabz" ;
+
     }
 }
 
