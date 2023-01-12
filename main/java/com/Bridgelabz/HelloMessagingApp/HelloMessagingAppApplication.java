dc313682878634d4228704bdff0f5d3ace27278c
dsdhumale <d.sdhumale@gmail.com>
2023-01-12 16:03:04 +0530

    [Devendra]Added: Use of get request method in Hello Messaging App

--- main/java/com/Bridgelabz/HelloMessagingApp/HelloMessagingAppApplication.java
+++ main/java/com/Bridgelabz/HelloMessagingApp/HelloMessagingAppApplication.java
@@ -9,5 +9,4 @@ public class HelloMessagingAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HelloMessagingAppApplication.class, args);
 	}
-
 }
--- main/java/com/Bridgelabz/HelloMessagingApp/controller/HelloMessagingAppController.java
+++ main/java/com/Bridgelabz/HelloMessagingApp/controller/HelloMessagingAppController.java
@@ -13,3 +13,4 @@ public class HelloMessagingAppController {
         return "Hello from Bridgelabz";
     }
 }
+
