405f699180014883ea13e6fd31beb39fd4f9bcf0
dsdhumale <d.sdhumale@gmail.com>
2023-01-12 17:06:00 +0530

    [Devendra]Added: REST API for post request method in Hello Messaging App

--- main/java/com/Bridgelabz/HelloMessagingApp/controller/HelloMessagingAppController.java
+++ main/java/com/Bridgelabz/HelloMessagingApp/controller/HelloMessagingAppController.java
@@ -2,9 +2,12 @@ package com.Bridgelabz.HelloMessagingApp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import com.Bridgelabz.HelloMessagingApp.controller.dto.User;
 
 @RestController
 @RequestMapping("/hello")
@@ -18,13 +21,16 @@ public class HelloMessagingAppController {
     @GetMapping("/query")
     public String queryParam(@RequestParam String name){
         return "Hello " + name + " from Bridgelabz" ;
-
     }
 
     @GetMapping("/path/{name}")
     public String nameParam(@PathVariable String name){
         return "Hello " + name + " !!!!" ;
+    }
 
+    @PostMapping("/user")
+    public String addUser(@RequestBody User user) {
+        return "Hello " + user.getFirstName() + " " + user.getLastName() +"!";
     }
 
 }
